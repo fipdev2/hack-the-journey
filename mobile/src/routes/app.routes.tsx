7065bfd812e7af196914eb8e93783749f697255a
Bruno Pavese <pavese.bruno@gmail.com>
2022-11-20 21:36:15 -0300

    Algumas alterações: tab bar, navegaçao entre frança e japão

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,76 +1,113 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import SignIn from '../screens/SignIn';
-import * as React from 'react';
-import Payment from '../screens/Payment';
-import SearchFlights from '../screens/SearchFlights';
-import FlightConfirm from '../screens/FlightConfirm';
-import MetaGuide from '../screens/MetaGuide';
-import Airport from '../screens/Airport';
-import ExploreFrance from '../screens/ExploreFrance';
-import AvailableFlights from '../screens/AvailableFlights';
-
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SignIn from "../screens/SignIn";
+import * as React from "react";
+import Payment from "../screens/Payment";
+import SearchFlights from "../screens/SearchFlights";
+import FlightConfirm from "../screens/FlightConfirm";
+import MetaGuide from "../screens/MetaGuide";
+import Airport from "../screens/Airport";
+import ExploreFrance from "../screens/ExploreFrance";
+import AvailableFlights from "../screens/AvailableFlights";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { useTheme } from "native-base";
+import { Platform } from "react-native";
+import ExploreJapan from "../screens/ExploreJapan";
 
 function AppRoutes() {
-    const { Navigator, Screen } = createBottomTabNavigator();
-    return (
-        <Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Screen
-                name='metaguide'
-                component={MetaGuide}
-            />
+  const { Navigator, Screen } = createBottomTabNavigator();
+  const { colors, sizes } = useTheme();
 
-            <Screen
-                name='airport'
-                component={Airport}
+  return (
+    <Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "rgba(127, 180, 228, 0.34)",
+          borderStyle: "solid",
+          borderTopWidth: 1,
+          borderTopColor: "rgba(255, 255, 255, 0.2)",
+          height: 60,
+        },
+        tabBarItemStyle: {
+          position: "relative",
+          top: Platform.OS === "ios" ? -10 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="metaguide"
+        component={MetaGuide}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="home" size={size} color={color} />
+            ) : (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+        }}
+      />
 
-            />
+      <Screen
+        name="airport"
+        component={Airport}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <MaterialIcons name="folder" size={size} color={color} />
+            ) : (
+              <MaterialIcons name="folder-open" size={size} color={color} />
+            ),
+        }}
+      />
 
-            <Screen
-                name='payment'
-                component={Payment}
-                options={{
-                    tabBarButton: () => null
-                }}
-            />
-            <Screen
-                name='searchflights'
-                component={SearchFlights}
-                options={{
-                    tabBarButton: () => null
-                }}
-            />
-            <Screen
-                name='flightconfirm'
-                component={FlightConfirm}
-                options={{
-                    tabBarButton: () => null
-                }}
-            />
-            <Screen
-                name='explorefrance'
-                component={ExploreFrance}
-                options={{
-                    tabBarButton: () => null
-                }}
-
-            />
-            <Screen
-                name='availableflights'
-                component={AvailableFlights}
-                options={{
-                    tabBarButton: () => null
-                }}
-
-            />
-
-
-        </Navigator>
-    );
+      <Screen
+        name="payment"
+        component={Payment}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="searchflights"
+        component={SearchFlights}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="flightconfirm"
+        component={FlightConfirm}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="explorefrance"
+        component={ExploreFrance}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="explorejapan"
+        component={ExploreJapan}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="availableflights"
+        component={AvailableFlights}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+    </Navigator>
+  );
 }
 
 export default AppRoutes;
--- mobile/src/screens/MetaGuide.tsx
+++ mobile/src/screens/MetaGuide.tsx
@@ -1,117 +1,96 @@
-import { VStack, Text, Image, Box, ZStack, Icon } from 'native-base';
-import Button from '../components/Button'
-import * as React from 'react';
-import MiniButton from '../components/MiniButton';
-import { Entypo } from '@expo/vector-icons';
-import { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-
-
+import { VStack, Text, Image, Box, ZStack, Icon } from "native-base";
+import Button from "../components/Button";
+import * as React from "react";
+import MiniButton from "../components/MiniButton";
+import { Entypo } from "@expo/vector-icons";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 function MetaGuide() {
-    const [airport, setAirport] = useState(false);
-    const [isLoading, setIsLoading] = useState(false)
-    const { navigate } = useNavigation()
-    return (
-        <VStack
-            flex={1}
-            px={6}
-            bg={{
-                linearGradient: {
-                    colors: ['darkpurple', 'middleblue'],
-                    start: [0, 0.3],
-                    end: [1, 1]
-                }
-            }}
-        >
-            <Text
-                fontFamily='heading'
-                color='white'
-                fontSize={32}
-                textAlign='center'
-                lineHeight={28}
-                mt={20}
-            >
-                Hello,{'\n'}
-                passenger!
-            </Text>
-            <Text
-                color='white'
-                fontSize={16}
-                textAlign='center'
-                lineHeight={16}
-                mt={4}>
-                Where do you want to go?
-            </Text>
+  const [airport, setAirport] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { navigate } = useNavigation();
+  return (
+    <VStack
+      flex={1}
+      px={6}
+      bg={{
+        linearGradient: {
+          colors: ["darkpurple", "middleblue"],
+          start: [0, 0.3],
+          end: [1, 1],
+        },
+      }}
+    >
+      <Text
+        fontFamily="heading"
+        color="white"
+        fontSize={32}
+        textAlign="center"
+        lineHeight={28}
+        mt={20}
+      >
+        Hello,{"\n"}
+        passenger!
+      </Text>
+      <Text
+        color="white"
+        fontSize={16}
+        textAlign="center"
+        lineHeight={16}
+        mt={4}
+      >
+        Where do you want to go?
+      </Text>
 
-            <ZStack
-                my={12}
-                h={362}
-                w='full'>
-                <Image
-                    source={require('../assets/map.png')}
+      <ZStack my={12} h={362} w="full" alignItems={'center'}>
+        <Image
+          source={require("../assets/map.png")}
+          alignItems={"center"}
+          w={341.94}
+          h={362}
+          alt="Map"
+        />
+        <MiniButton
+          onPress={() => setAirport(!airport)}
+          mt={90}
+          position={'relative'}
+          left={11}
+          text="Airport"
+          rightIcon={airport ? <Icon as={Entypo} name="check" /> : <Icon />}
+        />
+        <MiniButton
+          bgColor="gray.500"
+          mt={42}
+          position={'relative'}
+          right={0}
 
-                    w={341.94}
-                    h={362}
-                    alt='Map'
-                />
-                <MiniButton
-                    onPress={() => setAirport(!airport)}
-                    mt={24}
-                    ml={4}
-                    text='Airport'
-                    rightIcon={
-                        airport ?
-                            <Icon
+          text="Home"
+          rightIcon={<Icon as={Entypo} name="lock" />}
+        />
+      </ZStack>
 
-                                as={Entypo}
-                                name='check' />
-                            : <Icon />}
-                />
-                <MiniButton
-                    bgColor='gray.500'
-                    mt={16}
-                    ml={48}
-                    text='Home'
-                    rightIcon={
-                        <Icon
-                            as={Entypo}
-                            name='lock'
-                        />
-                    }
-                />
-            </ZStack>
+      <Box height="px" width="full" bgColor="white" opacity={0.22} mb={8} />
 
-            <Box
-                height='px'
-                width='full'
-                bgColor='white'
-                opacity={0.22}
-
-                mb={8}
-            />
-
-            {airport ?
-                <Button
-                    text='Go!'
-                    onPress={() => {
-                        setIsLoading(true)
-                        setAirport(false)
-                        setIsLoading(false)
-                        navigate('airport')
-
-                    }}
-                />
-                :
-                <Button
-                    bgColor='gray.500'
-                    onPress={null}
-                    text='Choose a destination!'
-                />
-            }
-        </VStack>
-
-    );
+      {airport ? (
+        <Button
+          text="Go!"
+          onPress={() => {
+            setIsLoading(true);
+            setAirport(false);
+            setIsLoading(false);
+            navigate("airport");
+          }}
+        />
+      ) : (
+        <Button
+          bgColor="gray.500"
+          onPress={null}
+          text="Choose a destination!"
+        />
+      )}
+    </VStack>
+  );
 }
 
 export default MetaGuide;
